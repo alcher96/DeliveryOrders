7904209b60d5806b40fd82b814b96eae5b490e31
alex <alcher96@mail.ru>
2024-10-25 16:45:20 +0300

    first

---
+++ Contracts/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using Entities.Models;
+
+namespace Contracts;
+
+public interface IOrderRepository
+{
+    Task<IEnumerable<Order>> GetAllOrdersAsync();
+
+    Task<Order> GetOrderAsync(Guid orderId, bool trackChanges);
+
+    Task<IEnumerable<Order>> GetFilteredOrdersAsync(DateTime intervalStart, DateTime intervalEnd);
+
+    void CreateOrder(Order order);
+
+    void DeleteOrder(Order order);
+}
---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Contracts;
+
+public interface IRepositoryManager
+{
+    IOrderRepository Order { get; }
+
+    Task SaveAsync();
+}
